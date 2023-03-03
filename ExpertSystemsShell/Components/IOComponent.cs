c385449322f9362d98807517816e200b1968dce1
JustKirpro <justkirpro@gmail.com>
2023-03-03 18:56:06 +0500

    Add binary serialization

--- ExpertSystemsShell/Components/IOComponent.cs
+++ ExpertSystemsShell/Components/IOComponent.cs
@@ -1,25 +1,70 @@
-﻿using Newtonsoft.Json;
-using System.IO;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Newtonsoft.Json;
 
 namespace ExpertSystemsShell.Components;
 
-public class IOComponent
+public class IoComponent
 {
+    private string _path = null!;
+
     private readonly JsonSerializerSettings _settings = new()
     {
         PreserveReferencesHandling = PreserveReferencesHandling.Objects
     };
 
-    public async Task<KnowledgeBase> LoadKnowledgeBase(string path)
+    /// <summary> 
+    /// Loads the knowledge base from a file at the given path.
+    /// </summary>
+    /// <param name="path"> Path to the knowledge base file. </param>
+    /// <returns> Loaded knowledge base. </returns>
+    /// <exception cref="IOException"></exception>
+    public KnowledgeBase LoadKnowledgeBase(string path)
     {
-        var json = await File.ReadAllTextAsync(path);
-        return JsonConvert.DeserializeObject<KnowledgeBase>(json, _settings)!;
+        var formatter = new BinaryFormatter();
+        using var stream = new FileStream(path, FileMode.OpenOrCreate);
+
+        try
+        {
+            var json = (string)formatter.Deserialize(stream);
+            return JsonConvert.DeserializeObject<KnowledgeBase>(json, _settings)!;
+        }
+        catch
+        {
+            throw new IOException("An error occurred while loading the knowledge base from a file");
+        }
     }
 
-    public async Task SaveKnowledgeBase(KnowledgeBase knowledgeBase, string path)
+    /// <summary>
+    /// Saves the knowledge base to a file at the given path.
+    /// </summary>
+    /// <param name="knowledgeBase"> Knowledge base to be saved to a file. </param>
+    /// <param name="path"> Path to the knowledge base file where the knowledge base will be saved. </param>
+    /// <exception cref="IOException"></exception>
+    public void SaveKnowledgeBase(KnowledgeBase knowledgeBase, string path)
     {
-        var json = JsonConvert.SerializeObject(knowledgeBase, Formatting.Indented, _settings);
-        await File.WriteAllTextAsync(path, json);
+        _path = path;
+        SaveKnowledgeBase(knowledgeBase);
+    }
+
+    /// <summary>
+    /// Saves the knowledge base to a file at the saved path. Path is saved after using <see cref="SaveKnowledgeBase(KnowledgeBase, string)"> overloaded method </see>
+    /// </summary>
+    /// <param name="knowledgeBase"> Knowledge base to be saved to a file. </param>
+    /// <exception cref="IOException"></exception>
+    public void SaveKnowledgeBase(KnowledgeBase knowledgeBase)
+    {
+        var formatter = new BinaryFormatter();
+        using var stream = new FileStream(_path, FileMode.OpenOrCreate);
+
+        try
+        {
+            var json = JsonConvert.SerializeObject(knowledgeBase, Formatting.Indented, _settings);
+            formatter.Serialize(stream, json);
+        }
+        catch
+        {
+            throw new IOException("An error occurred during serialization");
+        }
     }
 }
